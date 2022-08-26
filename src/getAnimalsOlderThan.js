fdb18fdcafc46f31dc549e578778b6715b104c6d
DouglasDainese <douglas_dainese@hotmail.com>
2022-08-26 18:25:38 -0300

    Resolução requisito 2, correção lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,10 +2,10 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, idade) {
-  const especies = data.species.filter((value) => animal.includes(value.name))[0].residents;
+  const especies = species.filter((value) => animal.includes(value.name))[0].residents;
   const idades = especies.every((element) => element.age >= idade);
   return idades;
 }
 
-getAnimalsOlderThan('tigers', 10);
+console.log(getAnimalsOlderThan('tigers', 10));
 module.exports = getAnimalsOlderThan;
