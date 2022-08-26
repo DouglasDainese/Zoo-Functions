4f2ca287d59fde033c19fe108957d125ef7a761e
DouglasDainese <douglas_dainese@hotmail.com>
2022-08-26 19:01:56 -0300

    Resolução requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = require('../data/zoo_data');
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find((value) => employeeName.includes(value.firstName)
+  || employeeName.includes(value.lastName));
 }
+
+console.log(getEmployeeByName('Burl'));
 
 module.exports = getEmployeeByName;
