6e3d6a976bb5be7ff2873bbe703c11087e74e3fb
DouglasDainese <douglas_dainese@hotmail.com>
2022-08-30 23:58:34 -0300

    Resolução requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const colabSpecie = data.employees.find((pessoa) => pessoa.id === id).responsibleFor[0];
+  const animalById = data.species.find((animal) => animal.id === colabSpecie).residents;
+  let animalMaisVelho = [];
+  let older = 0;
+  animalById.forEach((individuo) => {
+    if (individuo.age > older) {
+      older = individuo.age;
+      animalMaisVelho = Object.values(individuo);
+    }
+  });
+  return animalMaisVelho;
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
