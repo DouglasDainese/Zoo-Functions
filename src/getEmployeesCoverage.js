67383be53195c9ea38ff42131b2988fd01c2006f
DouglasDainese <douglas_dainese@hotmail.com>
2022-08-31 22:52:43 -0300

    Resolução requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -7,7 +7,7 @@ const data = require('../data/zoo_data');
 //   "locations": [ "NE", "NW" ],
 // },
 
-const todosColab = (param) => data.employees.map((colab) => ({
+const getAllColab = (param) => data.employees.map((colab) => ({
   id: colab.id,
   fullName: `${colab.firstName} ${colab.lastName}`,
   species: data.species.filter((animais) => colab.responsibleFor.includes(animais.id))
@@ -16,10 +16,47 @@ const todosColab = (param) => data.employees.map((colab) => ({
     .map((localização) => localização.location),
 }));
 
+const getColabByName = (param) => {
+  if (!data.employees.some((colaboradores) => colaboradores.firstName
+    .includes(param.name)) && !data.employees.some((colaboradores) => colaboradores.lastName
+    .includes(param.name))) {
+    throw new Error('Informações inválidas');
+  }
+  const colab = data.employees.find((valor) => param.name
+    .includes(valor.firstName) || param.name.includes(valor.lastName));
+  return {
+    id: colab.id,
+    fullName: `${colab.firstName} ${colab.lastName}`,
+    species: data.species.filter((animais) => colab.responsibleFor.includes(animais.id))
+      .map((name) => name.name),
+    locations: data.species.filter((animais) => colab.responsibleFor.includes(animais.id))
+      .map((localização) => localização.location),
+  };
+};
+
+const getColabById = (param) => {
+  if (!data.employees.some((colaboradores) => colaboradores.id
+    .includes(param.id))) {
+    throw new Error('Informações inválidas');
+  }
+  const colab = data.employees.find((valor) => param.id.includes(valor.id));
+  return {
+    id: colab.id,
+    fullName: `${colab.firstName} ${colab.lastName}`,
+    species: data.species.filter((animais) => colab.responsibleFor.includes(animais.id))
+      .map((name) => name.name),
+    locations: data.species.filter((animais) => colab.responsibleFor.includes(animais.id))
+      .map((localização) => localização.location),
+  };
+};
+
 function getEmployeesCoverage(param) {
-  if (param === undefined) return todosColab();
+  if (param === undefined) return getAllColab(param);
+  const parametro = Object.keys(param);
+  if (parametro[0] === 'name') return getColabByName(param);
+  if (parametro[0] === 'id') return getColabById(param);
 }
 
-console.log(getEmployeesCoverage());
+console.log(getEmployeesCoverage({ name: 'Strauss' }));
 
 module.exports = getEmployeesCoverage;
